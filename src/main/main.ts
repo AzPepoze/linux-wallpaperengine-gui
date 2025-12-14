f117f6501ea2c6f0e0829175c04c08bcb1c976b5
AzPepoze <pladumguy@gmail.com>
2025-12-14 21:23:25 +0700

    comment auto open devTools

--- src/main/main.ts
+++ src/main/main.ts
@@ -73,7 +73,7 @@ function createWindow() {
           // win.loadFile('dist/index.html')
           win.loadFile(path.join(process.env.DIST || "", "index.html"));
      }
-     win.webContents.openDevTools();
+     // win.webContents.openDevTools();
 }
 
 function createTray() {
