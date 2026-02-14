0c03e09bbbbb1d33b4f69a1190b7c719b0c76d98
sanfex <137943638+sanfex@users.noreply.github.com>
2026-02-14 17:54:54 +0100

    Added a button to refresh the list of wallpapers

---
+++ src/frontend/svelte/components/ui/Refresh.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+     import Button from "./Button.svelte";
+     import RefreshIcon from "../../icons/RefreshIcon.svelte";
+</script>
+
+<div class="refresh">
+     <Button variant="primary" on:click title="Refresh Wallpapers">
+          <RefreshIcon width="20" height="20" />
+     </Button>
+</div>
+
+<style>
+     :global(.btn) {
+          padding: 8px 8px !important;
+     }
+</style>
